40eb9b1c147ff71c498b664bf8057cdcd9c8d8c4
Inkzzz <denhamluke31@gmail.com>
2019-02-08 20:09:27 +0000

    Timed Actions
    Signed-off-by: Inkzzz <denhamluke31@gmail.com>

---
+++ src/main/java/io/inkstudios/timedactions/TimedActionsPlugin.java
@@ -0,0 +1,17 @@
+package io.inkstudios.timedactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TimedActionsPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	
+	}
+	
+	@Override
+	public void onDisable() {
+	
+	}
+	
+}
